14f6597cd8c8cc7c467b1f7677360981cf264156
Nick <nikolyaoooyartchuk@gmail.com>
2020-04-22 13:31:22 +0300

    Updates

---
+++ precache-manifest.ec913a251c574f574f349c693549fd24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8df15af064581ba1f33173cac1bed3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "468b0e9fb20bb1f83f65",
+    "url": "./static/css/main.722959fc.chunk.css"
+  },
+  {
+    "revision": "b23cd99607aafc7c9c87",
+    "url": "./static/js/2.bc76db86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bc76db86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "468b0e9fb20bb1f83f65",
+    "url": "./static/js/main.887e41f9.chunk.js"
+  },
+  {
+    "revision": "d34856cc17b978b4a115",
+    "url": "./static/js/runtime-main.9d7a78c6.js"
+  }
+]);
